f0a837e0d0ac6233f563344693e469f1a7ab3241
Guillaume Dussine <guillaume.dussine@telecomnancy.eu>
2021-04-11 16:27:02 +0200

    correction modele

--- src/main/java/fr/tncy/crown/service/impl/WordsListServiceImpl.java
+++ src/main/java/fr/tncy/crown/service/impl/WordsListServiceImpl.java
@@ -3,6 +3,7 @@ package fr.tncy.crown.service.impl;
 import fr.tncy.crown.model.WordsList;
 import fr.tncy.crown.repository.WordsListRepository;
 import fr.tncy.crown.service.WordsListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class WordsListServiceImpl implements WordsListService {
 
   private final WordsListRepository repository;
 
+  @Autowired
   public WordsListServiceImpl(WordsListRepository repository){
       this.repository = repository;
   }
